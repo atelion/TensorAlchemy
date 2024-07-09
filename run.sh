0b616e970b30e950b4ade6da534e7834f928fcdb
atelion <happilyai.meet@gmail.com>
2024-07-09 16:46:10 +0000

    add run.sh to run a miner

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python3 neurons/miners/StableMiner/main.py --wallet.name testcold --wallet.hotkey testhot --netuid 25 --subtensor.network test --axon.port 15368
+# python3 neurons/miners/StableMiner/main.py --wallet.name JJcold --wallet.hotkey JJb --netuid 26 --subtensor.network finney --axon.port 22033
+# python3 neurons/miners/StableMiner/main.py --wallet.name JJcold --wallet.hotkey JJwarm --netuid 26 --subtensor.network finney --axon.port 22037
+# python3 neurons/miners/StableMiner/main.py --wallet.name JJcold --wallet.hotkey JJa --netuid 26 --subtensor.network finney --axon.port 22024
+# CUDA_VISIBLE_DEVICES=3 python3 neurons/miners/StableMiner/main.py --wallet.name JJcold --wallet.hotkey JJhot --netuid 26 --subtensor.network finney --axon.port 22059
+
